73a6815c46d07682734302a7a6feffa394ae5e3f
Андрей Бабаев < >
2016-04-04 19:33:14 +0300

    паралакс, fixed header, видео превью, видео во всплывающем окне, галерея

--- js/js.js
+++ js/js.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
 
+	$('.js-video-prev').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false,
+		iframe: {
+	        patterns: {
+	            youtube: {
+	                src: '//www.youtube.com/embed/%id%?autoplay=1&rel=0'
+	            }
+	        }
+	    }
+	});
+
+	$('.js-video-prev').click(function () {
+	    return false;
+	});
+
 	$('.js-main-slider').slick({
 		prevArrow: '<button type="button" class="slick-prev"></button>',
 		nextArrow: '<button type="button" class="slick-next"></button>'
@@ -46,17 +67,43 @@ $(document).ready(function() {
 
 		//забираем идентификатор бока с атрибута href
 		var id  = $(this).attr('href'),
+			fixed_offset = 140,
 
 		//узнаем высоту от начала страницы до блока на который ссылается якорь
 			top = $(id).offset().top;
 		
 		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({scrollTop: top}, 1500);
+		$('body,html').animate({scrollTop: top - fixed_offset}, 1500);
 	});
 
 	$(".js-show-next-box-btn").click(function(){
 		$(this).toggleClass('active');
 		$(this).next('.hide-box').slideToggle();
+	});
+
+	var swidth=(window.innerWidth-$(window).width());
+	alert(swidth);
+
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Загрузка изображения #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">Не удалось загрузить изображение #%curr%</a>',
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		},
+		callbacks: {
+			open: function() { $('.header__layout').css('padding-right', swidth + "px"); }, 
+			close: function() { $('.header__layout').css('padding-right', 0); },
+		}
 	});
 
 });
